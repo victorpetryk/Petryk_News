d2ed69e9480e250b8d99b483d86a53a590e46261
Victor Petryk <victor.petryk@transoftgroup.com>
2018-08-28 12:05:34 +0300

    Added Data helper

---
+++ app/code/local/Tsg/News/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * News Data helper
+ *
+ * @category    Tsg
+ * @package     Tsg_News
+ * @author      Victor Petryk <victor.petryk@transoftgroup.com>
+ */
+
+class Tsg_News_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
